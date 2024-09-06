b072860a8ac8ca893173d371b41381d5ee907c50
AmeyGhogare <ameyghogare18@gmail.com>
2024-09-06 21:29:57 +0530

    calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
-    int num1, num2;
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
-    cout << "Addtion: " << num1 + num2 << endl;
-    cout << "subtraction: " << num1 - num2 << endl;
-    cout << "Multiplication : " << num1 * num2 << endl;
-    cout << "Division : " << num1 / num2 << endl;
+    double num1, num2;
+    char operation;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Choose an operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation)
+    {
+    case '+':
+        cout << "Result: " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << "Result: " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << "Result: " << num1 * num2 << endl;
+        break;
+    case '/':
+        cout << "Result: " << num1 / num2 << endl;
+        break;
+    default:
+        cout << "Invalid operation!" << endl;
+        break;
+    }
+
     return 0;
 }
